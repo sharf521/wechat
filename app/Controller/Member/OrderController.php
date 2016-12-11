96e51e5a72bc768e53b160a5a86186e923b11241
sharf521 <373455742@qq.com>
2016-12-11 22:19:41 +0800

    pay

--- app/Controller/Member/OrderController.php
+++ app/Controller/Member/OrderController.php
@@ -73,7 +73,7 @@ echo 33;
         $input = new \WxPayUnifiedOrder();
         $input->SetBody("test");
         $input->SetAttach("test");
-        $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+        $input->SetOut_trade_no(\WxPayConfig::MCHID.date("YmdHis"));
         $input->SetTotal_fee("1");
         $input->SetTime_start(date("YmdHis"));
         $input->SetTime_expire(date("YmdHis", time() + 600));
