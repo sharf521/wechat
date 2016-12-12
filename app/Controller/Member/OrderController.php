d8eb7dda018ee10a369af659f29ac3cf6adc6076
sharf <373455742@qq.com>
2016-12-12 16:11:24 +0800

    pay

--- app/Controller/Member/OrderController.php
+++ app/Controller/Member/OrderController.php
@@ -64,7 +64,7 @@ class OrderController extends MemberController
             'trade_type'       => 'JSAPI', // JSAPI，NATIVE，APP...
             'body'             => '支付订单',
             'out_trade_no'     => time().rand(10000,99999),
-            'total_fee'        => math(1,100,'*',2),
+            'total_fee'        => math($order->order_money,100,'*',2),
             'attach'=>$id,
             'openid'=>'on0aqs51hEudNQsGESP3GWEMYe78',
             'notify_url'       => "http://{$_SERVER['HTTP_HOST']}/index.php/wxOpen/payNotify/"
@@ -76,45 +76,10 @@ class OrderController extends MemberController
             $js = $app->js;
             $data['config']=$js->config(array('chooseWXPay','openAddress','checkJsApi'), true);
             $pay=$weChat->getPayParams($result->prepay_id);
-            print_r($pay);
             $data['pay']=$pay;
             $order->out_trade_no=$attributes['out_trade_no'];
             $order->save();
         }
-        /*require_once ROOT."/wxpay/lib/WxPay.Api.php";
-        require_once ROOT."/wxpay/example/WxPay.JsApiPay.php";
-        echo 111;
-        //①、获取用户openid
-        $tools = new \JsApiPay();
-        session()->set('target_url','http://'.$_SERVER['HTTP_HOST'].$this->self_url);
-        //$openId = $tools->GetOpenid();
-        $openid= (new WeChatOpen())->getOpenid();
-        echo $openid;
-        echo '<hr>'.'222';
-
-        //②、统一下单
-        $input = new \WxPayUnifiedOrder();
-        $input->SetBody("test");
-        $input->SetAttach("test");
-        $input->SetOut_trade_no(\WxPayConfig::MCHID.date("YmdHis"));
-        $input->SetTotal_fee("1");
-        $input->SetTime_start(date("YmdHis"));
-        $input->SetTime_expire(date("YmdHis", time() + 600));
-        $input->SetGoods_tag("test");
-        $input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
-        $input->SetTrade_type("JSAPI");
-        $input->SetOpenid($openid);
-        $order = \WxPayApi::unifiedOrder($input);
-        echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
-        printf_info($order);
-        $jsApiParameters = $tools->GetJsApiParameters($order);
-
-        //获取共享收货地址js函数参数
-        $editAddress = $tools->GetEditAddressParameters();
-
-        //③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
-
-        $data['jsApiParameters']=$jsApiParameters;*/
 
         $data['title_herder']='支付中。。';
 
