8fdd6342bb53fb5dc1f6497931ee7126d49965eb
sharf <373455742@qq.com>
2017-01-14 15:29:12 +0800

    center

--- app/Model/AccountCash.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Model;
-
-class AccountCash extends Model
-{
-    protected $table='account_cash';
-}
--- app/Model/App.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Model;
-
-class App extends Model
-{
-    protected $table = 'app';
-    
-
-}
