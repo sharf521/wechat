d2680ed76243a941f374e125309baf0ab79929a2
sharf <373455742@qq.com>
2017-03-02 18:30:12 +0800

    order

--- app/Controller/Home/IndexController.php
+++ app/Controller/Home/IndexController.php
@@ -14,6 +14,7 @@ class IndexController extends HomeController
     public function index(Goods $goods)
     {
         if($this->is_wap){
+            redirect('car');
             $data['goods_result']=$goods->where("status=1 and stock_count>0")->orderBy('id desc')->limit("0,10")->get();
         }else{
             $data['images']=array('\themes\default\images\ad1.jpg','\themes\default\images\ad2.jpg','\themes\default\images\ad3.jpg');
