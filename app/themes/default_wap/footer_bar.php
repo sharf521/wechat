5e01776784957ba55e3b099ecde8111f4e37add8
sharf <373455742@qq.com>
2017-04-11 17:54:37 +0800

    store

--- app/themes/default_wap/footer_bar.php
+++ app/themes/default_wap/footer_bar.php
@@ -3,11 +3,11 @@
         <i class="iconfont weui-tabbar__icon">&#xe64f;</i>
         <p class="weui-tabbar__label">首页</p>
     </a>
-    <a href="<?=url("goods/lists/?st_uid={$this->st_uid}")?>" class="weui-tabbar__item <? if($this->func=='lists'){echo 'weui-bar__item_on';}?>">
+    <a href="<?="{$this->home_url}/goods/lists/"?>" class="weui-tabbar__item <? if($this->func=='lists'){echo 'weui-bar__item_on';}?>">
         <i class="iconfont weui-tabbar__icon">&#xe600;</i>
         <p class="weui-tabbar__label">商品列表</p>
     </a>
-    <a href="<?=url("/cart/?st_uid={$this->st_uid}")?>" class="weui-tabbar__item <? if($this->control=='cart'){echo 'weui-bar__item_on';}?>">
+    <a href="<?="{$this->home_url}/cart"?>" class="weui-tabbar__item <? if($this->control=='cart'){echo 'weui-bar__item_on';}?>">
         <i class="iconfont weui-tabbar__icon" style="position: relative">&#xe698;
             <span class="weui-badge" id="cart_num"></span></i>
         <p class="weui-tabbar__label">购物车</p>
